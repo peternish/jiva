f22657d99bb6b1c54d858e80cf5f5c4ab4c0aae8
peternish <peter95613@gmail.com>
2022-02-21 19:04:51 +0700

    remove noncompliant code

--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,6 @@ import Image from 'next/image'
 import styles from '@styles/Home.module.css'
 
 export default function Home() {
-  function fun(a) {
-    var i = 10;
-    return i + a;
-    i++;             // Noncompliant; this is never executed
-  }
-
-  function fun(a) {
-    var i = 10;
-    return i + a;
-    i++;             // Noncompliant; this is never executed
-  }
   return (
     <div className={styles.container}>
       <Head>
