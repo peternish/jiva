315671b831ef009a60790b856fa6ddb3e44fc58e
peternish <peter95613@gmail.com>
2022-02-21 11:14:43 +0700

    chore: initial ci/cd setup

--- jiva_be/settings.py
+++ jiva_be/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = getenv("DEBUG") == "1"
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "localhost",
