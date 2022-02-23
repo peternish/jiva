054ee3f4bc9b4f47ddf98127e7a5cdcfce9e6174
peternish <peter95613@gmail.com>
2022-02-23 15:33:34 +0700

    setup urls for register

--- account/urls.py
+++ account/urls.py
@@ -1,3 +1,9 @@
+# django imports
 from django.urls import path
+from .views import register
 
-urlpatterns = []
+app_name='account'
+
+urlpatterns = [
+    path('register/', register, name='register')
+]
