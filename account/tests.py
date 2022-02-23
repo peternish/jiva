4b1181d952852d416fd68dd77c0ca5e1091fbc3d
peternish <peter95613@gmail.com>
2022-02-23 15:24:41 +0700

    [RED] add tests for register view

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,32 @@
+# Django imports
 from django.test import TestCase
+from django.http import HttpRequest
 
-# Create your tests here.
+# Rest imports
+from rest_framework.response import Response
+
+# other imports
+from . import views
+
+class UnitTest(TestCase):
+  def test_register_success(self):
+    request = HttpRequest()
+    payload = {
+      "email": "email@email.com", 
+      "password": "password",
+      "full_name": "This is a full name"
+    }
+    request.POST = payload
+    response = views.register(request).data
+    self.assertEqual(response.data, Response(payload).data)
+    self.assertEqual(response.status_code, 200)
+  
+  def test_register_missing_field(self):
+    request = HttpRequest()
+    payload = {
+      "email": "email@email.com", 
+      "password": "password"
+    }
+    request.POST = payload
+    response = views.register(request).data
+    self.assertEqual(response.status_code, 400)
