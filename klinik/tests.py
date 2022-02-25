0adc7d8b0597db5beba290be2ec9a0f41cd5e8b6
peternish <peter95613@gmail.com>
2022-02-25 10:48:18 +0700

    red: add test for klinik instantiation

--- klinik/tests.py
+++ klinik/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from unittest.mock import Mock
 
-# Create your tests here.
+from .models import Klinik, OwnerProfile
+
+
+class KlinikModelTest(TestCase):
+    def test_create_klinik(self):
+        klinik = Klinik('Lalita')
+        owner = Mock(spec=OwnerProfile)
+        owner._state = Mock()
+        klinik.owner = owner
+
+        self.assertEqual(klinik.name, 'Lalita')
+        self.assertEqual(klinik.owner.pk, owner.pk)
