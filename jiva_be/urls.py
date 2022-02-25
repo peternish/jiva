16331f4fe7fc8b703690e611c12fb0afc6b0a477
peternish <peter95613@gmail.com>
2022-02-25 07:19:59 +0700

    [REFACTOR] Rename url path

--- jiva_be/urls.py
+++ jiva_be/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('account.urls', namespace='account'))
+    path('account/', include('account.urls', namespace='account'))
 ]
