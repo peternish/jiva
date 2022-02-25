72677c244fd510c92db8f0f3ab0444ffbd5376c4
peternish <peter95613@gmail.com>
2022-02-25 20:25:08 +0700

    green: implement lihat values as links

--- components/pages/tenaga-medis/DashboardTenagaMedisPage.js
+++ components/pages/tenaga-medis/DashboardTenagaMedisPage.js
@@ -6,6 +6,7 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import Link from '@mui/material/Link';
 
 function DashboardTenagaMedisPage() {
   return (
@@ -24,7 +25,26 @@ function DashboardTenagaMedisPage() {
           </TableHead>
 
           <TableBody>
+            <TableRow>
+              <TableCell>Budi Budiman</TableCell>
+              <TableCell>Jakarta, 2 Januari 2001</TableCell>
+              <TableCell>081234567890</TableCell>
+              <TableCell><Link href="#">Lihat</Link></TableCell>
+            </TableRow>
 
+            <TableRow>
+              <TableCell>Budi Budiman</TableCell>
+              <TableCell>Jakarta, 2 Januari 2001</TableCell>
+              <TableCell>081234567890</TableCell>
+              <TableCell><Link href="#">Lihat</Link></TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell>Budi Budiman</TableCell>
+              <TableCell>Jakarta, 2 Januari 2001</TableCell>
+              <TableCell>081234567890</TableCell>
+              <TableCell><Link href="#">Lihat</Link></TableCell>
+            </TableRow>
           </TableBody>
         </Table>
       </TableContainer>
