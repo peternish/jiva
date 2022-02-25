a721eb798480f1bbbcc470543d714512e10b2994
peternish <peter95613@gmail.com>
2022-02-25 11:15:22 +0700

    green: fix syntax error for placeholder classes

--- klinik/models.py
+++ klinik/models.py
@@ -3,15 +3,20 @@ from django.db import models
 
 
 class Account(models.Model):
+    pass
 
 
 class Profile(models.Model):
+    pass
 
 
 class OwnerProfile(Profile):
+    pass
 
 
 class Klinik(models.Model):
+    pass
 
 
 class Cabang(models.Model):
+    pass
