2614fd50e2ee19b003e3df45296230231cf6b3c3
peternish <peter95613@gmail.com>
2022-02-25 10:57:51 +0700

    red: add test for klinik attribution

--- klinik/tests.py
+++ klinik/tests.py
@@ -5,11 +5,26 @@ from .models import Klinik, OwnerProfile
 
 
 class KlinikModelTest(TestCase):
-    def test_create_klinik(self):
+    def test_created_klinik_instace_of_Klinik_class(self):
         klinik = Klinik('Lalita')
         owner = Mock(spec=OwnerProfile)
         owner._state = Mock()
         klinik.owner = owner
 
-        self.assertEqual(klinik.name, 'Lalita')
+        self.assertIsInstance(klinik, Klinik)
+
+    def test_created_klinik_has_owner(self):
+        klinik = Klinik('Lalita')
+        owner = Mock(spec=OwnerProfile)
+        owner._state = Mock()
+        klinik.owner = owner
+
         self.assertEqual(klinik.owner.pk, owner.pk)
+
+    def test_created_klinik_has_name(self):
+        klinik = Klinik('Lalita')
+        owner = Mock(spec=OwnerProfile)
+        owner._state = Mock()
+        klinik.owner = owner
+
+        self.assertEqual(self.name, 'Lalita')
