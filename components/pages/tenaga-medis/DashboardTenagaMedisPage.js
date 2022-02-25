8a89224aa0d9953ee6e2cd36a799e00b970d44bc
peternish <peter95613@gmail.com>
2022-02-25 19:51:52 +0700

    green: add dashboard tenaga medis page main components

--- components/pages/tenaga-medis/DashboardTenagaMedisPage.js
+++ components/pages/tenaga-medis/DashboardTenagaMedisPage.js
@@ -1,9 +1,35 @@
 import Head from 'next/head'
+import Button from '@mui/material/Button';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 
 function DashboardTenagaMedisPage() {
   return (
     <main>
+      <h1>Daftar Tenaga Medis</h1>
 
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Nama</TableCell>
+              <TableCell>Tempat/Tanggal Lahir</TableCell>
+              <TableCell>NIK</TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+          </TableHead>
+
+          <TableBody>
+
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      <Button variant="contained">Tambah Tenaga Medis</Button>
     </main>
   )
 }
