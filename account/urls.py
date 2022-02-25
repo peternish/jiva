617a72b1dff78f5c00f29f4cc1628d8f54a95cdb
peternish <peter95613@gmail.com>
2022-02-25 09:22:02 +0700

    [GREEN] add refresh url and view

--- account/urls.py
+++ account/urls.py
@@ -5,6 +5,7 @@ from .views import register
 # rest imports
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
+    TokenRefreshView
 )
 
 app_name='account'
@@ -12,4 +13,5 @@ app_name='account'
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='refresh'),
 ]
