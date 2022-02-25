8112449cd661293d9a2adf4d425e7cdcc6e0eadb
peternish <peter95613@gmail.com>
2022-02-25 19:48:22 +0700

    red: add test to check dashboard tenaga medis page components

--- __tests__/tenaga-medis/index.test.jsx
+++ __tests__/tenaga-medis/index.test.jsx
@@ -10,4 +10,51 @@ describe('Dashboard Tenaga Medis', () => {
 
     expect(main).toBeInTheDocument()
   })
+
+
+  it('should have the page main heading', () => {
+    render(<DashboardTenagaMedis />)
+
+    const heading = screen.getByRole('heading', {
+      name: /Daftar Tenaga Medis/,
+    })
+
+    expect(heading).toBeInTheDocument()
+  })
+
+
+  it('should have the tenaga medis list as table', () => {
+    render(<DashboardTenagaMedis />)
+
+    const table = screen.getByRole('table')
+    const namaColumn = screen.getByRole('columnheader', {
+      name: /Nama/,
+    })
+    const ttlColumn = screen.getByRole('columnheader', {
+      name: /Tempat\/Tanggal Lahir/,
+    })
+    const nikColumn = screen.getByRole('columnheader', {
+      name: /NIK/,
+    })
+    const lihatColumn = screen.getByRole('columnheader', {
+      name: /^$/,
+    })
+
+    expect(table).toBeInTheDocument()
+    expect(namaColumn).toBeInTheDocument()
+    expect(ttlColumn).toBeInTheDocument()
+    expect(nikColumn).toBeInTheDocument()
+    expect(lihatColumn).toBeInTheDocument()
+  })
+
+
+  it('should have the tambah tenaga medis button', () => {
+    render(<DashboardTenagaMedis />)
+
+    const button = screen.getByRole('button', {
+      name: /Tambah Tenaga Medis/,
+    })
+
+    expect(button).toBeInTheDocument()
+  })
 })
