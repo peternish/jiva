6654cc73359036ec0968048d14e95b701d628372
peternish <peter95613@gmail.com>
2022-02-25 19:59:57 +0700

    refactor: split unit test for table into smaller unit test

--- __tests__/tenaga-medis/index.test.jsx
+++ __tests__/tenaga-medis/index.test.jsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react'
 import DashboardTenagaMedis from 'pages/tenaga-medis/index'
 import '@testing-library/jest-dom'
 
-describe('Dashboard Tenaga Medis', () => {
+describe('Dashboard Tenaga Medis Main Components', () => {
   it('should render', () => {
     render(<DashboardTenagaMedis />)
 
@@ -23,10 +23,32 @@ describe('Dashboard Tenaga Medis', () => {
   })
 
 
-  it('should have the tenaga medis list as table', () => {
+  it('should have the tenaga medis list table', () => {
     render(<DashboardTenagaMedis />)
 
     const table = screen.getByRole('table')
+    
+    expect(table).toBeInTheDocument()
+  })
+
+
+  it('should have the tambah tenaga medis button', () => {
+    render(<DashboardTenagaMedis />)
+
+    const button = screen.getByRole('button', {
+      name: /Tambah Tenaga Medis/,
+    })
+
+    expect(button).toBeInTheDocument()
+  })
+})
+
+
+
+describe('Tenaga medis list table properties', () => {
+  it('should have the appropriate columns', () => {
+    render(<DashboardTenagaMedis />)
+
     const namaColumn = screen.getByRole('columnheader', {
       name: /Nama/,
     })
@@ -39,22 +61,10 @@ describe('Dashboard Tenaga Medis', () => {
     const lihatColumn = screen.getByRole('columnheader', {
       name: /^$/,
     })
-
-    expect(table).toBeInTheDocument()
+    
     expect(namaColumn).toBeInTheDocument()
     expect(ttlColumn).toBeInTheDocument()
     expect(nikColumn).toBeInTheDocument()
     expect(lihatColumn).toBeInTheDocument()
-  })
-
-
-  it('should have the tambah tenaga medis button', () => {
-    render(<DashboardTenagaMedis />)
-
-    const button = screen.getByRole('button', {
-      name: /Tambah Tenaga Medis/,
-    })
-
-    expect(button).toBeInTheDocument()
   })
 })
