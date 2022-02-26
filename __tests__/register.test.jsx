dd227be4c1898dc15324e1579b1cfdeb553a83e7
peternish <peter95613@gmail.com>
2022-02-26 23:47:27 +0700

    red: add register page tests

---
+++ __tests__/register.test.jsx
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import Register from "@pages/register";
+import "@testing-library/jest-dom";
+
+describe("Register", () => {
+  it("renders register form", () => {
+    render(<Register />);
+    expect(screen.findByText("Daftar Pemilik Klinik")).toBeInTheDocument();
+
+    // Labels
+    expect(screen.getByLabelText("Email")).toBeInTheDocument();
+    expect(screen.getByLabelText("Password")).toBeInTheDocument();
+    expect(screen.getByLabelText("Nama Lengkap")).toBeInTheDocument();
+
+    // Input
+    expect(screen.getByPlaceholderText("jiva@gmail.com")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("Password")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("John Doe")).toBeInTheDocument();
+  });
+});
