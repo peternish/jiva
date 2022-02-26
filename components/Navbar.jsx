a06596691411c1c5a3c128ac94f961cbf088fc20
peternish <peter95613@gmail.com>
2022-02-26 20:32:50 +0700

    green: add navigation links

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,23 @@
 import styles from '@styles/Navbar.module.css'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
     <nav className={styles.nav}>
-        Jiva
+        <div className={styles.navBrand}>
+        <Link href='/'>
+            <a>Jiva</a>
+        </Link>
+        </div>
+
+        <div className={styles.navLinks}>
+          <Link href='/register'>
+            <a>Daftar</a>
+          </Link>
+          <Link href='/login'>
+            <a>Masuk</a>
+          </Link>
+        </div>
     </nav>
   )
 }
