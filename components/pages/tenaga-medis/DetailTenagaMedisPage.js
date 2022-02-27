29a3228391bd9c904b8b654d4c9f5e9ab35a3e17
peternish <peter95613@gmail.com>
2022-02-27 13:48:50 +0700

    green: add styles for data fields

--- components/pages/tenaga-medis/DetailTenagaMedisPage.js
+++ components/pages/tenaga-medis/DetailTenagaMedisPage.js
@@ -8,29 +8,35 @@ function DetailTenagaMedisPage() {
     <main className={styles.container}>
       <h1>Detil Informasi Tenaga Medis</h1>
 
-      <TextField 
-        variant="outlined" 
-        label="Nama"
-        defaultValue="Budi Budiman"
-      />
+      <Stack spacing={2} style={{marginTop: "2em", width: "50%",}}>
+        <TextField 
+          fullWidth 
+          variant="outlined" 
+          label="Nama"
+          defaultValue="Budi Budiman"
+        />
 
-      <TextField 
-        variant="outlined" 
-        label="Email"
-        defaultValue="budi.budiman@email.com"
-      />
+        <TextField 
+          fullWidth 
+          variant="outlined" 
+          label="Email"
+          defaultValue="budi.budiman@email.com"
+        />
 
-      <TextField 
-        variant="outlined" 
-        label="Nomor Telepon"
-        defaultValue="081234567890"
-      />
+        <TextField 
+          fullWidth 
+          variant="outlined" 
+          label="Nomor Telepon"
+          defaultValue="081234567890"
+        />
 
-      <TextField 
-        variant="outlined" 
-        label="Input Field"
-        defaultValue="Input Field"
-      />
+        <TextField 
+          fullWidth 
+          variant="outlined" 
+          label="Input Field"
+          defaultValue="Input Field"
+        />
+      </Stack>
 
       <Button variant="outlined">Hapus</Button>
       <Button variant="contained">Ubah</Button>
