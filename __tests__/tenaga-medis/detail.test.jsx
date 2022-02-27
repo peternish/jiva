3434d829fe231dd4678aae153c30fea413a1beb9
peternish <peter95613@gmail.com>
2022-02-27 13:33:09 +0700

    red: create test to check detail tenaga medis page main components

--- __tests__/tenaga-medis/detail.test.jsx
+++ __tests__/tenaga-medis/detail.test.jsx
@@ -10,4 +10,48 @@ describe('Detail tenaga medis page main components', () => {
 
     expect(main).toBeInTheDocument()
   })
+
+
+  it('should have the page main heading', () => {
+    render(<DetailTenagaMedis />)
+
+    const heading = screen.getByRole('heading', {
+      name: /Detil Informasi Tenaga Medis/,
+    })
+
+    expect(heading).toBeInTheDocument()
+  })
+
+
+  it('should have data fields', () => {
+    render(<DetailTenagaMedis />)
+
+    const fields = screen.getAllByRole('textbox')
+
+    fields.forEach((field) => {
+      expect(field).toBeInTheDocument()
+    })
+  })
+
+
+  it('should have the hapus button', () => {
+    render(<DetailTenagaMedis />)
+
+    const button = screen.getByRole('button', {
+      name: /Hapus/,
+    })
+
+    expect(button).toBeInTheDocument()
+  })
+
+  
+  it('should have the ubah button', () => {
+    render(<DetailTenagaMedis />)
+
+    const button = screen.getByRole('button', {
+      name: /Ubah/,
+    })
+
+    expect(button).toBeInTheDocument()
+  })
 })
