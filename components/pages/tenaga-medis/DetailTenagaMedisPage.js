b217dc3eab5eb2a60cdeafae8319756768c57de4
peternish <peter95613@gmail.com>
2022-02-27 13:35:59 +0700

    green: add main components for detail tenaga medis page

--- components/pages/tenaga-medis/DetailTenagaMedisPage.js
+++ components/pages/tenaga-medis/DetailTenagaMedisPage.js
@@ -1,8 +1,39 @@
 import styles from '@styles/Home.module.css'
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack'
+import Button from '@mui/material/Button'
 
 function DetailTenagaMedisPage() {
   return (
     <main className={styles.container}>
+      <h1>Detil Informasi Tenaga Medis</h1>
+
+      <TextField 
+        variant="outlined" 
+        label="Nama"
+        defaultValue="Budi Budiman"
+      />
+
+      <TextField 
+        variant="outlined" 
+        label="Email"
+        defaultValue="budi.budiman@email.com"
+      />
+
+      <TextField 
+        variant="outlined" 
+        label="Nomor Telepon"
+        defaultValue="081234567890"
+      />
+
+      <TextField 
+        variant="outlined" 
+        label="Input Field"
+        defaultValue="Input Field"
+      />
+
+      <Button variant="outlined">Hapus</Button>
+      <Button variant="contained">Ubah</Button>
     </main>
   )
 }
