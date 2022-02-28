2836ab97f63fdfec013053567f1fce9ecffd61ef
peternish <peter95613@gmail.com>
2022-02-28 08:42:19 +0700

    red: set base file on pages,components, and make basic tests for them

---
+++ components/pages/pengaturan-pengguna/index.js
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+import styled from "styled-components"
+
+const Dashboard = () => {
+
+  return (
+        <>
+        </>
+  );
+};
+
+export default Dashboard;
---
+++ pages/pengaturan-pengguna/index.js
@@ -0,0 +1,5 @@
+import DashboardPengaturanPengguna from "components/pages/pengaturan-pengguna/"
+
+const Dashboard = () => <DashboardPengaturanPengguna/>
+
+export default Dashboard
