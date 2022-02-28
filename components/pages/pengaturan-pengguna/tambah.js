d4bd839130ba05a33c77da40f81c12372bf34a03
peternish <peter95613@gmail.com>
2022-02-28 09:13:44 +0700

    red: implement code to pass the tests on tambah except the form

--- components/pages/pengaturan-pengguna/tambah.js
+++ components/pages/pengaturan-pengguna/tambah.js
@@ -2,11 +2,42 @@ import { useState } from "react"
 
 import styled from "styled-components"
 
+// component imports
+import { Formik, Field, Form } from "formik"
+import Button from "@mui/material/Button"
+
+const TambahForm = () => {
+  return (
+    <>
+    </>
+  );
+};
+
 const Tambah = () => {
 
   return (
-        <>
-        </>
+    <main>
+    <h1>Pengaturan Pengguna</h1>
+    <Formik
+      initialValues={{
+        email: "",
+        password: "",
+        input_value: "",
+      }}
+      validate={(values) => {}}
+      onSubmit={(values, { setSubmitting }) => {}}
+    >
+      {() => (
+        <Form className="form">
+          <TambahForm />
+        </Form>
+      )}
+    </Formik>
+    
+
+    <Button variant="outlined">Batal</Button>
+    <Button type="submit" variant="contained" >Simpan</Button>
+    </main>
   );
 };
 
