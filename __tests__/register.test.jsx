acb6b6e91597aee36bb674eba0d11d5c6ab0c374
peternish <peter95613@gmail.com>
2022-03-01 21:42:06 +0700

    red: add tests

--- __tests__/register.test.jsx
+++ __tests__/register.test.jsx
@@ -1,20 +1,54 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Register from "@pages/register";
 import "@testing-library/jest-dom";
 
-describe("Register", () => {
-  it("renders register form", () => {
-    render(<Register />);
-    expect(screen.getByText("Daftar Pemilik Klinik")).toBeInTheDocument();
+describe("<Register/>", () => {
+  beforeEach(() => {
+    render(<Register/>)
+  })
 
-    // Labels
-    expect(screen.getByLabelText("Email")).toBeInTheDocument();
-    expect(screen.getByLabelText("Password")).toBeInTheDocument();
-    expect(screen.getByLabelText("Nama Lengkap")).toBeInTheDocument();
+  describe("Renders first page correctly", () => {
+    it("renders owner register form", () => {
+      expect(screen.getByText("Daftar Pemilik Klinik")).toBeInTheDocument();
+  
+      // Labels
+      expect(screen.getByLabelText("Email")).toBeInTheDocument();
+      expect(screen.getByLabelText("Password")).toBeInTheDocument();
+      expect(screen.getByLabelText("Nama Lengkap")).toBeInTheDocument();
+  
+      // Input
+      expect(screen.getByPlaceholderText("jiva@gmail.com")).toBeInTheDocument();
+      expect(screen.getByPlaceholderText("Password")).toBeInTheDocument();
+      expect(screen.getByPlaceholderText("John Doe")).toBeInTheDocument();
+    });
+  
+    it("renders buttons correctly", () => {
+      expect(screen.getByText("Batal")).toBeInTheDocument()
+      expect(screen.getByText("Lanjut")).toBeInTheDocument()
+    })
+  })
 
-    // Input
-    expect(screen.getByPlaceholderText("jiva@gmail.com")).toBeInTheDocument();
-    expect(screen.getByPlaceholderText("Password")).toBeInTheDocument();
-    expect(screen.getByPlaceholderText("John Doe")).toBeInTheDocument();
-  });
+  describe("Renders second page correctly", () => {
+    beforeEach(() => {
+      const nextButton = screen.getByText("Lanjut")
+      fireEvent.click(nextButton)
+    })
+
+    it("renders owner register form", () => {
+      expect(screen.getByText("Daftar Klinik")).toBeInTheDocument();
+  
+      // Labels
+      expect(screen.getByLabelText("Nama Klinik")).toBeInTheDocument();
+      expect(screen.getByLabelText("Surat Izin Klinik")).toBeInTheDocument();
+  
+      // Input
+      expect(screen.getByPlaceholderText("Jiva")).toBeInTheDocument();
+    });
+  
+    it("renders buttons correctly", () => {
+      expect(screen.getByText("Kembali")).toBeInTheDocument()
+      expect(screen.getByText("Daftar")).toBeInTheDocument()
+      expect(screen.getByText("Daftar").getAttribute("type")).toBe("submit")
+    })
+  })
 });
