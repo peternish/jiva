b18a5d607aa8e402beadc38b8710a50a69c230cf
peternish <peter95613@gmail.com>
2022-03-01 09:36:48 +0700

    red: fix some syntax errors in tests

--- __tests__/register.test.jsx
+++ __tests__/register.test.jsx
@@ -5,11 +5,16 @@ import "@testing-library/jest-dom";
 describe("Register", () => {
   it("renders register form", () => {
     render(<Register />);
-    expect(screen.findByText("Daftar Pemilik Klinik")).toBeInTheDocument();
+    expect(screen.getByText("Daftar Pemilik Klinik")).toBeInTheDocument();
 
     // Labels
     expect(screen.getByLabelText("Email")).toBeInTheDocument();
     expect(screen.getByLabelText("Password")).toBeInTheDocument();
     expect(screen.getByLabelText("Nama Lengkap")).toBeInTheDocument();
+
+    // Input
+    expect(screen.getByPlaceholderText("jiva@gmail.com")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("Password")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("John Doe")).toBeInTheDocument();
   });
 });
