f7c0f21f2d7cd8f49b2d6e4ce38f65f16bcbf796
peternish <peter95613@gmail.com>
2022-03-02 23:20:23 +0700

    green: add view methods

--- klinik/views.py
+++ klinik/views.py
@@ -1,3 +1,22 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def get_all_cabang(request: HttpRequest) -> HttpResponse:
+    pass
+
+
+def get_cabang(request: HttpRequest) -> HttpResponse:
+    pass
+
+
+def create_cabang(request: HttpRequest) -> HttpResponse:
+    pass
+
+
+def update_cabang(request: HttpRequest) -> HttpResponse:
+    pass
+
+
+def remove_cabang(request: HttpRequest) -> HttpResponse:
+    pass
