97b6d083510a2d619f792cd8f5690eddd3c27e7d
peternish <peter95613@gmail.com>
2022-03-02 10:04:23 +0700

    red: add test for rendering sidebar

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import Navbar from 'components/Navbar'
+import Sidebar from 'components/Sidebar'
 
 describe('Navbar', () => {
   it('renders a navbar', () => {
@@ -17,5 +18,15 @@ describe('Navbar', () => {
     const links = screen.getAllByRole('link')
 
     expect(links).toHaveLength(3)
+  })
+})
+
+describe('Sidebar', () => {
+  it('renders a sidebar', () => {
+    render(<Sidebar/>)
+
+    const sidebar = screen.getByTestId('sidebar')
+
+    expect(sidebar).toBeInTheDocument()
   })
 })
