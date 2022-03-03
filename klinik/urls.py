046b003c64c6f23ef1811bd27e0fde9ed8483cbf
peternish <peter95613@gmail.com>
2022-03-03 14:06:10 +0700

    green: implement get all cabang

--- klinik/urls.py
+++ klinik/urls.py
@@ -1,18 +1,17 @@
 # django imports
-from xml.etree.ElementInclude import include
-from django.urls import path
-import views
+from django.urls import path, include
+from klinik import views
 
 app_name = "klinik"
 
 klinik_patterns = [
-    path("all/", views.get_all_cabang, name="get_all_cabang"),
-    path("fetch/", views.get_cabang, name="get_cabang"),
-    path("register/", views.create_cabang, name="create_cabang"),
-    path("revise/", views.update_cabang, name="update_cabang"),
-    path("remove/", views.remove_cabang, name="remove_cabang"),
+    path("all", views.get_all_cabang, name="get_all_cabang"),
+    path("fetch", views.get_cabang, name="get_cabang"),
+    path("register", views.create_cabang, name="create_cabang"),
+    path("revise", views.update_cabang, name="update_cabang"),
+    path("remove", views.remove_cabang, name="remove_cabang"),
 ]
 
 urlpatterns = [
-    path("cabang/", include(klinik_patterns, namespace="cabang")),
+    path("cabang/", include(klinik_patterns)),
 ]
--- klinik/views.py
+++ klinik/views.py
@@ -1,22 +1,34 @@
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.http import HttpRequest
+from klinik.serializers import CabangSerializer
+from klinik.models import Cabang, Klinik
+from rest_framework import status
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework.decorators import api_view
 
 
-def get_all_cabang(request: HttpRequest) -> HttpResponse:
+@api_view(["GET"])
+def get_all_cabang(request: Request) -> Response:
+    id_klinik = request.query_params.get("klinik")
+    # TODO: Handle klinik None
+    query = Cabang.objects.filter(klinik__id=id_klinik)
+    serialized = CabangSerializer(
+        query, many=True, context={'request': request})
+    return Response(status=status.HTTP_200_OK, data=serialized)
+
+
+def get_cabang(request: Request) -> Response:
     pass
 
 
-def get_cabang(request: HttpRequest) -> HttpResponse:
+def create_cabang(request: Request) -> Response:
     pass
 
 
-def create_cabang(request: HttpRequest) -> HttpResponse:
+def update_cabang(request: Request) -> Response:
     pass
 
 
-def update_cabang(request: HttpRequest) -> HttpResponse:
-    pass
-
-
-def remove_cabang(request: HttpRequest) -> HttpResponse:
+def remove_cabang(request: Request) -> Response:
     pass
