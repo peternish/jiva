714a75fe1224881d3475e8937d280ba389ec27a2
peternish <peter95613@gmail.com>
2022-03-04 12:24:35 +0700

    refactor: revert function based implementation

--- jiva_be/urls.py
+++ jiva_be/urls.py
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("account/", include("account.urls", namespace="account")),
-    path("klinik/", include("klinik.urls", namespace="klinik")),
 ]
