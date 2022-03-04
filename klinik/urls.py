42bdefd85e31a0a11ed84b10b910025abbce898c
peternish <peter95613@gmail.com>
2022-03-04 12:56:59 +0700

    refactor: reformat uri assignment

--- klinik/urls.py
+++ klinik/urls.py
@@ -2,9 +2,11 @@ from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from klinik import views
 
+app_name = "klinik"
+
 cabang_patterns = [
-    path('cabang/', views.CabangListApi.as_view()),
-    path('cabang/<int:pk>/', views.CabangDetailApi.as_view()),
+    path('', views.CabangListApi.as_view()),
+    path('<int:pk>/', views.CabangDetailApi.as_view()),
 ]
 
 cabang_patterns = format_suffix_patterns(cabang_patterns)
