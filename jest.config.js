8279bc49061e59f0b723b196c72e8875ecea8f72
peternish <peter95613@gmail.com>
2022-03-04 15:28:18 +0700

    chore: add moduleNameMapper to jest config

--- jest.config.js
+++ jest.config.js
@@ -12,7 +12,18 @@ const customJestConfig = {
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ['node_modules', '<rootDir>/'],
+  moduleNameMapper: {
+    // Handle module aliases (this will be automatically configured for you soon)
+    "^@components/(.*)$": "<rootDir>/components/$1",
+    "^@pages/(.*)$": "<rootDir>/pages/$1",
+    "^@api/(.*)$": "<rootDir>/api/$1",
+    "^@redux/(.*)$": "<rootDir>/redux/$1",
+    "^@styles/(.*)$": "<rootDir>/styles/$1",
+    "^@utils/(.*)$": "<rootDir>/utils/$1",
+  },
   testEnvironment: 'jest-environment-jsdom',
+  "collectCoverage": true,
+  collectCoverageFrom: ["components/**", "pages/[^_]*/**", "pages/[^_]*"],
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
