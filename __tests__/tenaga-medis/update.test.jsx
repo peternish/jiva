b25e2d4da330f8234db7763824e2522349c5a802
peternish <peter95613@gmail.com>
2022-03-04 13:36:08 +0700

    red: add test to check update tenaga medis main components

--- __tests__/tenaga-medis/update.test.jsx
+++ __tests__/tenaga-medis/update.test.jsx
@@ -10,4 +10,48 @@ describe('Update tenaga medis page main components', () => {
 
     expect(main).toBeInTheDocument()
   })
+
+
+  it('should have the page main heading', () => {
+    render(<DetailTenagaMedis />)
+
+    const heading = screen.getByRole('heading', {
+      name: /Update Tenaga Medis/,
+    })
+
+    expect(heading).toBeInTheDocument()
+  })
+
+
+  it('should have data fields', () => {
+    render(<DetailTenagaMedis />)
+
+    const fields = screen.getAllByRole('textbox')
+
+    fields.forEach((field) => {
+      expect(field).toBeInTheDocument()
+    })
+  })
+
+
+  it('should have the batal button', () => {
+    render(<DetailTenagaMedis />)
+
+    const button = screen.getByRole('button', {
+      name: /Batal/,
+    })
+
+    expect(button).toBeInTheDocument()
+  })
+
+  
+  it('should have the simpan button', () => {
+    render(<DetailTenagaMedis />)
+
+    const button = screen.getByRole('button', {
+      name: /Simpan/,
+    })
+
+    expect(button).toBeInTheDocument()
+  })
 })
