ad25ad43b54633342bd7828d50969991b79d290e
peternish <peter95613@gmail.com>
2022-03-04 13:20:02 +0700

    red: add test to check create tenaga medis page main components

--- __tests__/tenaga-medis/create.test.jsx
+++ __tests__/tenaga-medis/create.test.jsx
@@ -10,4 +10,48 @@ describe('Create tenaga medis page main components', () => {
 
     expect(main).toBeInTheDocument()
   })
+
+  
+  it('should have the page main heading', () => {
+    render(<CreateTenagaMedis />)
+
+    const heading = screen.getByRole('heading', {
+      name: /Tambah Tenaga Medis/,
+    })
+
+    expect(heading).toBeInTheDocument()
+  })
+
+
+  it('should have data fields', () => {
+    render(<CreateTenagaMedis />)
+
+    const fields = screen.getAllByRole('textbox')
+
+    fields.forEach((field) => {
+      expect(field).toBeInTheDocument()
+    })
+  })
+
+
+  it('should have the batal button', () => {
+    render(<CreateTenagaMedis />)
+
+    const button = screen.getByRole('button', {
+      name: /Batal/,
+    })
+
+    expect(button).toBeInTheDocument()
+  })
+
+  
+  it('should have the tambah button', () => {
+    render(<CreateTenagaMedis />)
+
+    const button = screen.getByRole('button', {
+      name: /Tambah/,
+    })
+
+    expect(button).toBeInTheDocument()
+  })
 })
