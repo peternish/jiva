4d8d78e31293a82d269150e9eea2e548bc1f4936
peternish <peter95613@gmail.com>
2022-03-04 09:28:00 +0700

    feat: add toast for feedback to user

--- pages/_app.js
+++ pages/_app.js
@@ -5,12 +5,17 @@ import Layout from "@components/Layout";
 import { Provider } from "react-redux";
 import { store } from "@redux/store";
 
+// toast
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
+      <ToastContainer />
     </Provider>
   );
 }
