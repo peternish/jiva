a0d61663bd60de0de4bcba40921072bfb8932ee7
peternish <peter95613@gmail.com>
2022-03-09 00:47:25 +0700

    red: modify test for dynamic detail tenaga medis page content testing

--- __tests__/tenagaMedis/detail.test.jsx
+++ __tests__/tenagaMedis/detail.test.jsx
@@ -1,11 +1,29 @@
 import { render, screen } from '@testing-library/react';
-import DetailTenagaMedis from '@pages/tenaga-medis/detail';
+import DetailTenagaMedis from '@pages/tenaga-medis/detail/[id]';
 import { Provider } from "react-redux";
 import { store } from "@redux/store";
 import '@testing-library/jest-dom';
+import { setTenagaMedis } from "@redux/modules/tenagaMedis";
+
+const useRouter = jest.spyOn(require('next/router'), 'useRouter');
 
 describe('DetailTenagaMedis', () => {
-  beforeEach(() => {
+  beforeEach( async () => {
+    await store.dispatch(setTenagaMedis(
+      [
+        {
+          id: 1,
+          name: "Anthony",
+          tempatTanggalLahir: "Berlin, 12 Maret 1976",
+          nik: "1234567890",
+        },
+      ]
+    ));
+
+    useRouter.mockImplementationOnce(() => ({
+      query: { id: 1 },
+    }));
+
     render(
       <Provider store={store}>
         <DetailTenagaMedis />
