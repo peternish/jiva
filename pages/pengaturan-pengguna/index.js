9cce5a7b8521525c6cda16f81483701e713118dc
peternish <peter95613@gmail.com>
2022-03-08 21:20:44 +0700

    green: comply to the new page/component layout

--- pages/pengaturan-pengguna/index.js
+++ pages/pengaturan-pengguna/index.js
@@ -1,5 +1,84 @@
-import DashboardPengaturanPengguna from "components/pages/pengaturan-pengguna/"
+// component imports
+import Head from "next/head";
+import Button from "@mui/material/Button"
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import Link from '@mui/material/Link'
+import Layout from "@components/Layout";
+import Container from '@mui/material/Container'
 
-const Dashboard = () => <DashboardPengaturanPengguna/>
+const PengaturanPenggunaTable = () => {
+  return (
+    <>
+    <TableContainer>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Nama</TableCell>
+            <TableCell>Email</TableCell>
+            <TableCell>No.Telp</TableCell>
+            <TableCell>Role</TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+        </TableHead>
 
-export default Dashboard
+        <TableBody>
+          <TableRow>
+            <TableCell>Sata Ganaga</TableCell>
+            <TableCell>examels@gomail.com</TableCell>
+            <TableCell>081234567890</TableCell>
+            <TableCell>Moderator</TableCell>
+            <TableCell>:</TableCell>
+          </TableRow>
+
+          <TableRow>
+            <TableCell>Sata Ganaga</TableCell>
+            <TableCell>examels@gomail.com</TableCell>
+            <TableCell>081234567890</TableCell>
+            <TableCell>Moderator</TableCell>
+            <TableCell>:</TableCell>
+          </TableRow>
+
+          <TableRow>
+            <TableCell>Sata Ganaga</TableCell>
+            <TableCell>examels@gomail.com</TableCell>
+            <TableCell>081234567890</TableCell>
+            <TableCell>Moderator</TableCell>
+            <TableCell>:</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
+    </>
+  );
+};
+
+const Dashboard = () => {
+
+  return (
+        <Layout navType="sidebar">
+          <Head>
+            <title>Pengaturan Pengguna</title>
+            <meta name="pengaturan pengguna" content="pengaturan pengguna" />
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+          <Container>
+            <h1>Pengaturan Pengguna</h1>
+
+            <PengaturanPenggunaTable />
+
+            <Link href="/pengaturan-pengguna/tambah">
+              <Button variant="contained" >
+                Tambah Pengguna
+              </Button>
+            </Link>
+          </Container>
+        </Layout>
+  );
+};
+
+export default Dashboard;
--- pages/pengaturan-pengguna/tambah.js
+++ pages/pengaturan-pengguna/tambah.js
@@ -1,5 +1,69 @@
-import TambahPengguna from "components/pages/pengaturan-pengguna/tambah"
+// component imports
+import Head from "next/head";
+import { Formik, Field, Form } from "formik"
+import Button from "@mui/material/Button"
+import TextInput from "components/common/TextInput"
+import Layout from "@components/Layout";
+import Container from '@mui/material/Container'
 
-const Tambah = () => <TambahPengguna/>
+const TambahForm = () => {
+  return (
+    <>
+      <TextInput
+        name="email"
+        type="email"
+        label="Email"
+        placeholder="jiva@goog.com"
+      />
+      <TextInput
+        name="password"
+        type="password"
+        label="Password"
+        placeholder="password"
+      />
+      <TextInput
+        name="input_value"
+        type="text"
+        label="Input Value"
+        placeholder="Input Value"
+      />
+    </>
+  );
+};
 
-export default Tambah
+const fields = {
+  email: "",
+  password: "",
+  input_value: "",
+};
+
+const Tambah = () => {
+
+  return (
+        <Layout navType="sidebar">
+          <Head>
+            <title>Tambah | Pengaturan Pengguna</title>
+            <meta name="tambah pengaturan pengguna" content="tambah pengaturan pengguna" />
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+          <Container>
+            <h1>Tambah Pengguna</h1>
+            <Formik
+              initialValues={{fields}}
+              validate={(values) => {}}
+              onSubmit={(values, { setSubmitting }) => {}}
+              >
+              {() => (
+                <Form className="form" role="form">
+                  <TambahForm />
+                </Form>
+              )}
+            </Formik>
+            <Button variant="outlined">Batal</Button>
+            <Button type="submit" variant="contained" >Simpan</Button>
+          </Container>
+        </Layout>
+  );
+};
+
+export default Tambah;
