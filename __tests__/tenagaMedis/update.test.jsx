0e71730a9220aba52e46a051896d11c5f00835d2
peternish <peter95613@gmail.com>
2022-03-09 01:22:58 +0700

    red: modify test to check dynamic update tenaga medis page content

--- __tests__/tenagaMedis/update.test.jsx
+++ __tests__/tenagaMedis/update.test.jsx
@@ -1,11 +1,29 @@
 import { render, screen } from '@testing-library/react';
-import UpdateTenagaMedis from '@pages/tenaga-medis/update';
+import UpdateTenagaMedis from '@pages/tenaga-medis/update/[id]';
 import { Provider } from "react-redux";
 import { store } from "@redux/store";
 import '@testing-library/jest-dom';
+import { setTenagaMedis } from "@redux/modules/tenagaMedis";
+
+const useRouter = jest.spyOn(require('next/router'), 'useRouter');
 
 describe('UpdateTenagaMedis', () => {
-  beforeEach(() => {
+  beforeEach( async () => {
+    await store.dispatch(setTenagaMedis(
+      [
+        {
+          id: 1,
+          name: "Anthony",
+          tempatTanggalLahir: "Berlin, 12 Maret 1976",
+          nik: "1234567890",
+        },
+      ]
+    ));
+
+    useRouter.mockImplementationOnce(() => ({
+      query: { id: 1 },
+    }));
+
     render(
       <Provider store={store}>
         <UpdateTenagaMedis />
