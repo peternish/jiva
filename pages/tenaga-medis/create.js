6f0c0669ece909641b11d135e435a546a7c06bf5
peternish <peter95613@gmail.com>
2022-03-11 00:47:13 +0700

    green: implement new form fields and form flow

--- pages/tenaga-medis/create.js
+++ pages/tenaga-medis/create.js
@@ -1,53 +1,77 @@
 // components
-import TextField from '@mui/material/TextField';
-import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import Layout from '@components/Layout';
+import Stack from '@mui/material/Stack';
+import { Formik, Form } from "formik";
+import TextInput from "@components/common/TextInput";
 
 // styles
 import layoutStyles from '@styles/Layout.module.css';
 
 function CreateTenagaMedis() {
+  const fields = {
+    fullName: "",
+    email: "",
+    password: "",
+  };
+
   return (
     <main>
       <Layout>
         <div className={layoutStyles.containerWithSidebar}>
           <h1>Tambah Tenaga Medis</h1>
+          
+          <Formik
+            initialValues={fields}
+            validate={(values) => {
+              const errors = {};
+              const ERR_MESSAGE = "Input ini wajib diisi";
+              Object.keys(fields).forEach((key) => {
+                if (!values[key]) errors[key] = ERR_MESSAGE;
+              });
+              return errors;
+            }}
+            onSubmit={(values) => {
+              try {
+                console.log(values);
+              } catch (err) {
+                console.log(err);
+              }
+            }}
+          >
+            {({ isValid, errors }) => (
+              <Form>
+                <TextInput 
+                  name="fullName"
+                  type="text"
+                  label="Nama Lengkap"
+                  placeholder="dr. Budi Budiman, Sp.A."
+                  error={errors.fullName}
+                />
 
-          <Stack spacing={2}>
-            <TextField 
-              fullWidth 
-              variant="outlined" 
-              label="Nama"
-              placeholder="Budi Budiman"
-            />
+                <TextInput 
+                  name="email"
+                  type="email"
+                  label="Email"
+                  placeholder="budi.budiman@email.com"
+                  error={errors.email}
+                />
 
-            <TextField 
-              fullWidth 
-              variant="outlined" 
-              label="Email"
-              placeholder="budi.budiman@email.com"
-            />
+                <TextInput 
+                  name="password"
+                  type="password"
+                  label="Password"
+                  placeholder=""
+                  error={errors.password}
+                />
 
-            <TextField 
-              fullWidth 
-              variant="outlined" 
-              label="Nomor Telepon"
-              placeholder="081234567890"
-            />
-
-            <TextField 
-              fullWidth 
-              variant="outlined" 
-              label="Input Field"
-              placeholder="Input Field"
-            />
-          </Stack>
-
-          <Stack spacing={2} direction="row">
-            <Button variant="outlined">Batal</Button>
-            <Button variant="contained">Tambah</Button>
-          </Stack>
+                <Stack spacing={2} direction="row">
+                  <Button variant="outlined">Batal</Button>
+                  <Button variant="contained" type="submit" disabled={!isValid}>Tambah</Button>
+                </Stack>
+              </Form>
+            )}
+          </Formik>
         </div>
       </Layout>
     </main>
