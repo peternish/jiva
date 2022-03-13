ac550339c611e7686e3973ddb8cf0a87010bde59
peternish <peter95613@gmail.com>
2022-03-13 13:10:29 +0700

    green: implemented staf CRUD

--- account/urls.py
+++ account/urls.py
@@ -1,14 +1,27 @@
 # django imports
 from django.urls import path
 from .views import register
+from account import views
+from django.urls import path, include
 
 # rest imports
+from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+
+
 app_name = "account"
+
+staf_patterns = [
+    path("<slug:location>/", views.StafListApi.as_view(), name="staf-list"),
+    path("id/<int:pk>/", views.StafApi.as_view(), name="staf-detail"),
+]
+
+staf_patterns = format_suffix_patterns(staf_patterns)
 
 urlpatterns = [
     path("register/", register, name="register"),
     path("login/", TokenObtainPairView.as_view(), name="login"),
     path("token/refresh/", TokenRefreshView.as_view(), name="refresh"),
+    path("staf/", include(staf_patterns))
 ]
--- klinik/admin.py
+++ klinik/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(OwnerProfile)
+admin.site.register(StafProfile)
 admin.site.register(Klinik)
 admin.site.register(Cabang)
 admin.site.register(Profile)
