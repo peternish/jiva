46ad3a65024590fa313b44458313cb8e51b5bf3b
peternish <peter95613@gmail.com>
2022-03-14 19:38:12 +0700

    green: implement dynamic form validation errors

--- pages/klinik/[idKlinik]/[idCabang]/tenaga-medis/create.js
+++ pages/klinik/[idKlinik]/[idCabang]/tenaga-medis/create.js
@@ -43,10 +43,16 @@ function CreateTenagaMedis() {
             initialValues={{ ...fields }}
             validate={(values) => {
               const errors = {};
-              const ERR_MESSAGE = "Input ini wajib diisi";
-              Object.keys(fields).forEach((key) => {
-                if (!values[key]) errors[key] = ERR_MESSAGE;
-              });
+              
+              if (!values.email) {
+                errors.email = "Email wajib diisi";
+              } else if (!/.+@.+\..+/.test(values.email)) {
+                errors.email = "Masukkan email yang valid";
+              }
+
+              if (!values.password) errors.password = "Password wajib diisi";
+              if (!values.fullName) errors.fullName = "Nama lengkap wajib diisi";
+              
               return errors;
             }}
             onSubmit={(values) => {
--- pages/klinik/[idKlinik]/[idCabang]/tenaga-medis/update/[id].js
+++ pages/klinik/[idKlinik]/[idCabang]/tenaga-medis/update/[id].js
@@ -45,10 +45,9 @@ function UpdateTenagaMedis() {
               }}
               validate={(values) => {
                 const errors = {};
-                const ERR_MESSAGE = "Input ini wajib diisi";
-                ["fullName", "email"].forEach((key) => {
-                  if (!values[key]) errors[key] = ERR_MESSAGE;
-                });
+                
+                if (!values.fullName) errors.fullName = "Nama lengkap wajib diisi";
+                
                 return errors;
               }}
               onSubmit={(values) => {
