5dd70aef8925e66814821269a0c3c96fcb418c52
peternish <peter95613@gmail.com>
2022-03-28 23:14:19 +0700

    green: implement JadwalTenagaMedis model

--- jadwal/models.py
+++ jadwal/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from klinik.models import TenagaMedisProfile
 
-# Create your models here.
+
+class JadwalTenagaMedis(models.Model):
+    DAY_CHOICES = (
+        ("mon", "Monday"),
+        ("tue", "Tuesday"),
+        ("wed", "Wednesday"),
+        ("thu", "Thursday"),
+        ("fri", "Friday"),
+        ("sat", "Saturday"),
+        ("sun", "Sunday"),
+    )
+
+    tenaga_medis = models.ForeignKey(
+        TenagaMedisProfile, 
+        on_delete=models.CASCADE, 
+        related_name="jadwal_tenaga_medis"
+    )
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    quota = models.IntegerField()
+    day = models.CharField(max_length=3, choices=DAY_CHOICES)
+
+    def __str__(self) -> str:
+        return f"{self.tenaga_medis.account}'s Jadwal"
