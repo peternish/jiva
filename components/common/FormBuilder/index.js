bb4c91167e3ebe26a5e27da1f198e47bfe38e0ba
peternish <peter95613@gmail.com>
2022-04-03 19:01:08 +0700

    perf: change styled div to regular css

--- components/common/FormBuilder/index.js
+++ components/common/FormBuilder/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import Button from "@mui/material/Button";
 import CSS from "./CSS";
 
--- components/common/URLPreview.jsx
+++ components/common/URLPreview.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import Tooltip from "@mui/material/Tooltip";
-import styled from "styled-components";
+import styles from "@styles/URLPreview.module.css";
 
 export default function URLPreview({ URL }) {
   const [tooltipTitle, setTooltipTitle] = useState("Copy URL");
@@ -11,25 +11,12 @@ export default function URLPreview({ URL }) {
     setTooltipTitle("URL Copied!");
   };
 
-  const Style = styled.span`
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      column-gap: 1em;
-      border: 1px solid rgba(0, 0, 0, 0.23);
-      border-radius: 4px;
-      padding: 0.5em 1em;
-      margin-top: 1em;
-      min-width: 30%;
-      max-width: fit-content;
-`;
-
   return (
-    <Style data-testid={'URLPreview'}>
+    <div className={styles.previewURL} data-testid={'URLPreview'}>
       {URL}
       <Tooltip title={tooltipTitle} onClose={() => setTooltipTitle("Copy URL")}>
         <ContentCopyIcon onClick={copyToClipboard} cursor="pointer" />
       </Tooltip>
-    </Style>
+    </div>
   );
 }
