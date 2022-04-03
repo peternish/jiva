08b22f61197344980d18a6234f3724b31b791e48
peternish <peter95613@gmail.com>
2022-04-04 01:48:40 +0700

    refactor: add success message

--- pages/form/[idKlinik]/[idCabang]/[idForm]/index.jsx
+++ pages/form/[idKlinik]/[idCabang]/[idForm]/index.jsx
@@ -1,18 +1,18 @@
+import axios from "axios"
+import { useState } from "react"
 import styled from "styled-components";
 import { Formik, Form, Field } from "formik";
 import FormRender from "@components/common/FormRender";
 import TextInput from "@components/common/TextInput";
 import Layout from "@components/DynamicForm/Layout";
 import Card from "@mui/material/Card";
-import axios from "axios"
 import constants from "@api/constants"
 
 const CSS = styled.div`
   height: 100%;
-  display: flex;S
+  display: flex;
   justify-content: center;
   align-items: center;
-  width: 100%;
 
   input {
     width: 100% !important;
@@ -72,53 +72,54 @@ const CSS = styled.div`
 
 export async function getServerSideProps({ params, res }) {
   const { idCabang } = params
-  // const jadwalTenagaMedis = [
-  //   {
-  //     "id": 2,
-  //     "tenaga_medis": {
-  //       "account": {
-  //         "id": 2,
-  //         "full_name": "TM 2",
-  //         "email": "tm2@klinik99.com",
-  //         "date_joined": "2022-03-31T12:49:11.378628Z",
-  //         "last_login": "2022-03-31T12:49:11.378628Z",
-  //         "role": "tenaga_medis",
-  //         "cabang": 1,
-  //         "klinik": 1
-  //       },
-  //       "sip": "https://django-surat-izin-klinik-jiva.s3.amazonaws.com/static/HW2204B4.txt"
-  //     },
-  //     "start_time": "10:00:00",
-  //     "end_time": "12:00:00",
-  //     "quota": 5,
-  //     "day": "mon"
-  //   },
-  //   {
-  //     "id": 3,
-  //     "tenaga_medis": {
-  //       "account": {
-  //         "id": 3,
-  //         "full_name": "TM 3",
-  //         "email": "tm3@klinik99.com",
-  //         "date_joined": "2022-04-02T10:43:33.797983Z",
-  //         "last_login": "2022-04-02T10:43:33.797983Z",
-  //         "role": "tenaga_medis",
-  //         "cabang": 1,
-  //         "klinik": 1
-  //       },
-  //       "sip": "https://django-surat-izin-klinik-jiva.s3.amazonaws.com/static/HW2204B4.txt"
-  //     },
-  //     "start_time": "11:00:00",
-  //     "end_time": "14:00:00",
-  //     "quota": 5,
-  //     "day": "mon"
-  //   }
-  // ]
+  const jadwalTenagaMedis = [
+    {
+      "id": 2,
+      "tenaga_medis": {
+        "account": {
+          "id": 2,
+          "full_name": "TM 2",
+          "email": "tm2@klinik99.com",
+          "date_joined": "2022-03-31T12:49:11.378628Z",
+          "last_login": "2022-03-31T12:49:11.378628Z",
+          "role": "tenaga_medis",
+          "cabang": 1,
+          "klinik": 1
+        },
+        "sip": "https://django-surat-izin-klinik-jiva.s3.amazonaws.com/static/HW2204B4.txt"
+      },
+      "start_time": "10:00:00",
+      "end_time": "12:00:00",
+      "quota": 5,
+      "day": "mon"
+    },
+    {
+      "id": 3,
+      "tenaga_medis": {
+        "account": {
+          "id": 3,
+          "full_name": "TM 3",
+          "email": "tm3@klinik99.com",
+          "date_joined": "2022-04-02T10:43:33.797983Z",
+          "last_login": "2022-04-02T10:43:33.797983Z",
+          "role": "tenaga_medis",
+          "cabang": 1,
+          "klinik": 1
+        },
+        "sip": "https://django-surat-izin-klinik-jiva.s3.amazonaws.com/static/HW2204B4.txt"
+      },
+      "start_time": "11:00:00",
+      "end_time": "14:00:00",
+      "quota": 5,
+      "day": "mon"
+    }
+  ]
 
+  let jadwalByDoctor
   try {
-    const jadwalTenagaMedis = await axios.get(`${constants?.API_BASE_URL}/klinik/cabang/${idCabang}/dform/`)
+    // const jadwalTenagaMedis = await axios.get(`${constants?.API_BASE_URL}/klinik/cabang/${idCabang}/dform/`)
     // const jadwalTenagaMedis = await getJadwalTenagaMedisList({ idCabang: idCabang })
-    const jadwalByDoctor = jadwalTenagaMedis.reduce((r, a) => {
+    jadwalByDoctor = jadwalTenagaMedis.reduce((r, a) => {
       r[a.tenaga_medis.account.id] = r[a.tenaga_medis.account.id] || []
       r[a.tenaga_medis.account.id].push(a)
       return r
@@ -128,48 +129,48 @@ export async function getServerSideProps({ params, res }) {
   }
 
 
-  // return {
-  //   props: {
-  //     namaKlinik: "Klinik Example",
-  //     jadwal: jadwalByDoctor,
-  //     fields: [
-  //       {
-  //         type: "text",
-  //         required: false,
-  //         label: "Field 1",
-  //         className: "form-control",
-  //         name: "text-1648102772033-0",
-  //         access: false,
-  //         subtype: "text",
-  //       },
-  //       {
-  //         type: "text",
-  //         required: true,
-  //         label: "Field 2",
-  //         className: "form-control",
-  //         name: "text-1648102772980-0",
-  //         access: false,
-  //         subtype: "text",
-  //       },
-  //     ],
-  //   }
-  // }
-  const {
-    data: { cabang_id, klinik, fields },
-  } = await jivaAPI.dynamicForm.fetch({ cabang_id, form_id: idForm })
   return {
     props: {
-      idKlinik,
-      idCabang,
-      namaKlinik: klinik.name,
-      fields
+      namaKlinik: "Klinik Example",
+      jadwal: jadwalByDoctor,
+      fields: [
+        {
+          type: "text",
+          required: false,
+          label: "Field 1",
+          className: "form-control",
+          name: "text-1648102772033-0",
+          access: false,
+          subtype: "text",
+        },
+        {
+          type: "text",
+          required: true,
+          label: "Field 2",
+          className: "form-control",
+          name: "text-1648102772980-0",
+          access: false,
+          subtype: "text",
+        },
+      ],
     }
   }
+  // const {
+  //   data: { cabang_id, klinik, fields },
+  // } = await jivaAPI.dynamicForm.fetch({ cabang_id, form_id: idForm })
+  // return {
+  //   props: {
+  //     idKlinik,
+  //     idCabang,
+  //     namaKlinik: klinik.name,
+  //     fields
+  //   }
+  // }
 }
 
 const RegistrationForm = ({ namaKlinik, fields, jadwal }) => {
   console.log(jadwal)
-  // const [tenaga_medis, setTenagamedis] = useState(null);
+  const [submitted, setSubmitted] = useState(false);
 
   const mandatoryFields = {
     nik: "",
@@ -188,60 +189,71 @@ const RegistrationForm = ({ namaKlinik, fields, jadwal }) => {
             {namaKlinik}
           </h1>
 
-          <p>{JSON.stringify(Object.keys(jadwal), null, 2)}</p>
+          {submitted
+            ? (
+              <p>Reservasi anda telah tersimpan! Mohon datang tepat waktu!</p>
+            )
+            : (
+              <Formik
+                initialValues={{ ...mandatoryFields }}
+                validate={(values) => {
+                  const errors = {}
+                  if (!values.nik) errors.email = "NIK wajib diisi"
+                  if (!values.tenaga_medis) errors.tenaga_medis = "Tenaga medis wajib dipilih"
+                  if (!values.jadwal) errors.jadwal = "Jadwal wajib dipilih"
+                  return errors
+                }}
+                onSubmit={(values, { setSubmitting }) => {
+                  console.log(values)
+                  setSubmitting(true)
+                  setSubmitted(true)
+                }}
+              >
+                {({ errors, isSubmitting, submitForm, values, setSubmitting, isValid, validateForm, setFieldValue }) => (
+                  <>
+                    <Form>
+                      <TextInput
+                        name="nik"
+                        type="text"
+                        label="NIK"
+                        placeholder="1234567890"
+                        error={errors.nik}
+                      />
+                      <label htmlFor={"tenagamedis"}>Jadwal dokter</label>
+                      <Field as="select" id="tenagamedis" name="tenaga_medis">
+                        <option value="" disabled={true} hidden={true}>Pilih tenaga medis</option>
+                        {Object.keys(jadwal).map((e) => (
+                          <option value={e} key={e}>{e}</option>
+                        ))}
+                      </Field>
+                      <Field as="select" disabled={!values.tenaga_medis} name="jadwal">
+                        <option value="" disabled={true} hidden={true}>Pilih jadwal pertemuan</option>
+                        {values.tenaga_medis && (
+                          jadwal[values.tenaga_medis].map((e, i) => (
+                            <option value={e.day + " " + e.start_time} key={e.id}>
+                              {e.day}
+                              {" "}
+                              {e.start_time}
+                            </option>
+                          )))}
+                      </Field>
+                    </Form>
+                    <FormRender
+                      schema={fields}
+                      submit={async (e) => {
+                        setFieldValue("fields", e);
+                        await submitForm(e)
+                        // setSubmitted(true)
+                      }}
+                      isSubmitting={isSubmitting}
+                      isValid={isValid}
+                    />
+                  </>
+                )}
+              </Formik>
+            )
+          }
 
-          <Formik
-            initialValues={{ ...mandatoryFields }}
-            validate={(values) => {
-              const errors = {}
-              if (!values.nik) errors.email = "NIK wajib diisi"
-              if (!values.tenaga_medis) errors.tenaga_medis = "Tenaga medis wajib dipilih"
-              if (!values.jadwal) errors.jadwal = "Jadwal wajib dipilih"
-              return errors
-            }}
-            onSubmit={(values, { setSubmitting }) => {
-              console.log(values)
-              setSubmitting(true)
-            }}
-          >
-            {({ errors, isSubmitting, submitForm, values, setSubmitting, isValid, validateForm, setFieldValue }) => (
-              <>
-                <Form>
-                  <TextInput
-                    name="nik"
-                    type="text"
-                    label="NIK"
-                    placeholder="1234567890"
-                    error={errors.nik}
-                  />
-                  <label htmlFor={"tenagamedis"}>Jadwal dokter</label>
-                  <Field as="select" id="tenagamedis" name="tenaga_medis">
-                    <option value="" disabled={true} hidden={true}>Pilih tenaga medis</option>
-                    {Object.keys(jadwal).map((e) => (
-                      <option value={e} key={e}>{e}</option>
-                    ))}
-                  </Field>
-                  <Field as="select" disabled={!values.tenaga_medis} name="jadwal">
-                    <option value="" disabled={true} hidden={true}>Pilih jadwal pertemuan</option>
-                    {values.tenaga_medis && (
-                      jadwal[values.tenaga_medis].map((e, i) => (
-                        <option value={e.day + " " + e.start_time} key={e.id}>
-                          {e.day}
-                          {" "}
-                          {e.start_time}
-                        </option>
-                      )))}
-                  </Field>
-                </Form>
-                <FormRender
-                  schema={fields}
-                  submit={(e) => { setFieldValue("fields", e); submitForm(e) }}
-                  isSubmitting={isSubmitting}
-                  isValid={isValid}
-                />
-              </>
-            )}
-          </Formik>
 
         </Card>
       </CSS>
