f2b45135981d490a06b5c14b9ae7f63288b95140
peternish <peter95613@gmail.com>
2022-04-14 14:12:27 +0700

    chore: add urls mapping

---
+++ rekammedis/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework.urlpatterns import format_suffix_patterns
+from rekammedis import views
+
+app_name = "ehr"
+
+urlpatterns = [
+    # path("<str:nik>/", views_todo, name="rekam-medis")
+    # path("<int:id>/", views_detail_todo, name="detil-rekam-medis")
+]
