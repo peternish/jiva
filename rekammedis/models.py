eadf4a122e70d9a068a027bfa706ed5f15766244
peternish <peter95613@gmail.com>
2022-04-14 12:15:22 +0700

    chore: implement model based on database design

--- rekammedis/models.py
+++ rekammedis/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from klinik.models import TenagaMedisProfile
 
-# Create your models here.
+
+class Pasien(models.Model):
+    nik = models.CharField(max_length=20, unique=True)
+    full_name = models.TextField("Nama Lengkap")
+
+    def __str__(self) -> str:
+        return self.nik
+
+
+class RekamanMedis(models.Model):
+    fields = models.JSONField("Fields", default=list)
+    time_created = models.DateField(auto_now_add=True)
+    author = models.ForeignKey(TenagaMedisProfile, on_delete=models.CASCADE)
+    patient = models.ForeignKey(Pasien, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.patient + " " + self.time_created
