e7907cea12ace8b1d6df8c6abd0ca48b1ce247cc
peternish <peter95613@gmail.com>
2022-04-18 19:59:55 +0700

    refactor: refactor dictionary handling

--- klinik/utils.py
+++ klinik/utils.py
@@ -7,7 +7,7 @@ def send_confirmation_email(fields):
     subject = "Pendaftaran Berhasil"
     message = "Terima kasih!\nPendaftaran Anda telah berhasil dilakukan!"
     from_email = settings.EMAIL_HOST_USER
-    recipient_list = [ get_fields_attribute(fields, "email") ]
+    recipient_list = [ get_fields_attribute_value(fields, "email") ]
     send_mail(
         subject=subject, 
         message=message, 
@@ -16,9 +16,9 @@ def send_confirmation_email(fields):
         fail_silently=False
     )
 
-def get_fields_attribute(fields, attribute):
+def get_fields_attribute_value(fields, attribute_name):
     for field in fields:
-        field_attr = field.get(attribute)
-        if field_attr is not None:
-            return field_attr
+        field_name = field.get("name")
+        if field_name.lower() == attribute_name:
+            return field["value"]
     return None
