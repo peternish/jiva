523347c39c2f227376aa1c704644abd38fa5d0d2
peternish <peter95613@gmail.com>
2022-04-20 21:53:50 +0700

    feat: beautify confirmation email

--- klinik/utils.py
+++ klinik/utils.py
@@ -1,19 +1,27 @@
 # django imports
 from django.core.mail import send_mail
+from django.utils.html import strip_tags
 from django.conf import settings
 
 
 def send_confirmation_email(fields):
+    context = {
+        "nik" : "123321123321",
+        "tanggal" : "21 April 2022",
+        "jam_kedatangan" : "13:00",
+        "dokter" : "dr. Budi Budiman, Sp.A",
+    }
     subject = "Pendaftaran Berhasil"
-    message = "Terima kasih!\nPendaftaran Anda telah berhasil dilakukan!"
     from_email = settings.EMAIL_HOST_USER
     recipient_list = [ get_fields_attribute_value(fields, "email") ]
+    html_message = compose_email_body(context)
     send_mail(
         subject=subject, 
-        message=message, 
+        message=strip_tags(html_message), 
         from_email=from_email, 
         recipient_list=recipient_list,
-        fail_silently=False
+        fail_silently=False,
+        html_message=html_message
     )
 
 def get_fields_attribute_value(fields, attribute_name):
@@ -22,3 +30,12 @@ def get_fields_attribute_value(fields, attribute_name):
         if field_name.lower() == attribute_name:
             return field["value"]
     return None
+
+def compose_email_body(context):
+    with open("klinik/templates/mail_template.html") as template_file:
+        template = template_file.read()
+        template = template.replace("{{ nik }}", context["nik"])
+        template = template.replace("{{ tanggal }}", context["tanggal"])
+        template = template.replace("{{ jam_kedatangan }}", context["jam_kedatangan"])
+        template = template.replace("{{ dokter }}", context["dokter"])
+    return template
