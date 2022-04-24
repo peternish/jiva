828865469bf0077fae196df30f729437fbd81e0b
peternish <peter95613@gmail.com>
2022-04-24 20:34:17 +0700

    fix: delete account when profile is deleted

--- klinik/signals.py
+++ klinik/signals.py
@@ -1,9 +1,9 @@
 # Django imports
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
 # Model imports
-from .models import Cabang, DynamicForm
+from .models import Cabang, DynamicForm, Profile
 
 # Handle cabang creation
 @receiver(post_save, sender=Cabang)
@@ -12,3 +12,7 @@ def cabang_creation_handler(sender, instance, created, **kwargs):
         cabang = instance
         for formtype in DynamicForm.formtypes:
             DynamicForm(cabang=cabang, formtype=formtype).save()
+
+@receiver(post_delete, sender=Profile)
+def delete_related_user(sender, instance: Profile, **kwargs):
+    instance.account.delete()
