fd4ac281bcbf9548524b73b1844c3814a7447dd6
peternish <peter95613@gmail.com>
2022-05-17 23:07:56 +0700

    red: add test to display patient list

---
+++ __tests__/rekamanMedis/pasien-list.test.jsx
@@ -0,0 +1,37 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import RekamanMedisGlobal from "@pages/rekaman-medis";
+
+// redux
+import { Provider } from "react-redux";
+import { store } from "@redux/store";
+import { setPasienList } from "@redux/modules/rekamanMedis";
+
+// utils
+import { SAMPLE_PASIEN } from "@utils/testUtils/constants"
+
+describe("<RekamanMedisGlobal/> empty", () => {
+    beforeEach(async () => {
+
+        await store.dispatch(setPasienList([SAMPLE_PASIEN]))
+
+        render(
+            <Provider store={store}>
+                <RekamanMedisGlobal />
+            </Provider>
+        );
+    });
+
+    it("renders title correctly", () => {
+        expect(screen.getByText("Daftar Pasien")).toBeInTheDocument();
+    });
+
+    it("renders patient data correctly", () => {
+        expect(screen.getByText("sample patient")).toBeInTheDocument();
+        expect(screen.getByText("123")).toBeInTheDocument();
+    });
+
+    it('renders buttons', () => {
+        expect(screen.getByText("Tambah Pasien")).toBeInTheDocument()
+    });
+});
